9e16da070d8310be63fbe942c3c6cf0c98d713e8
carlos <carlos@MacBook-Pro-de-Luis.local>
2020-03-19 18:48:11 -0600

    junit

--- src/test/java/buscaminas/BuscaminasTest.java
+++ src/test/java/buscaminas/BuscaminasTest.java
@@ -2,18 +2,28 @@ package buscaminas;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class BuscaminasTest {
 
 	@Test
 	public void testBuscarMina() {
-		fail("Not yet implemented");
+		List<String> y = new ArrayList<>();
+		y.add("*");
+		y.add(".");
+		List<List<String>> campo = new ArrayList<>();
+		campo.add(y);
+		String r = Buscaminas.buscarMina(1, 1, campo);
+		assertEquals("1", r);
 	}
 
-	@Test
-	public void testLeerArchivo() {
-		fail("Not yet implemented");
+	@Test(expected = Exception.class)
+	public void testLeerArchivo() throws IOException {
+		Buscaminas.leerArchivo("archivo.txt");
 	}
 
 }
